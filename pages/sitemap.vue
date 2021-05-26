b8b0ce823185c08a9f3205c03266570ad953acab
root <root@185-20-226-198.cloudvps.regruhosting.ru>
2021-05-26 07:41:09 +0300

    Перезаписано получение карты сайта

--- pages/sitemap.vue
+++ pages/sitemap.vue
@@ -1,22 +1,22 @@
 <template>
-<section>
+<section class="content_block">
 <h1>Карта сайта</h1>
   <ul>
-    <li v-for="(item, name) in data">
-      <nuxt-link :to="item.link">{{name}}</nuxt-link>
+    <li v-for="(item, name) in sitemap">
+      <nuxt-link class="default__link" :to="item.link">{{name}}</nuxt-link>
       <ul>
         <li  v-for="(item2, name2) in item.child">
-          <nuxt-link :to="item2.link">{{name2}}</nuxt-link>
+          <nuxt-link class="default__link" :to="item2.link">{{name2}}</nuxt-link>
           <ul>
             <li  v-for="(item3, name3) in item2.child">
 
               <details>
                 <summary>
-                  <nuxt-link :to="item3.link">{{name3}}</nuxt-link>
+                  <nuxt-link class="default__link" :to="item3.link">{{name3}}</nuxt-link>
                 </summary>
                 <ul>
                   <li  v-for="(item4, name4) in item3.child">
-                    <nuxt-link :to="item4">{{name4}}</nuxt-link>
+                    <nuxt-link class="default__link" :to="item4">{{name4}}</nuxt-link>
                   </li>
                 </ul>
               </details>
@@ -39,11 +39,25 @@
 export default {
   name: "sitemap",
 
-  async asyncData({ params, $axios }) {
-    const result = await $axios.$get('https://dev.unicso.ru/v1/config/sitemap')
-    var data = result.body
-    return { data }
+  data(){
+    return{
+      sitemap:false
+    }
+  },
+  mounted()
+  {
+    this.load_sitemap()
+  },
+  methods:{
+    async load_sitemap()
+    {
+        const result = await this.$axios.$get('https://dev.unicso.ru/v1/config/sitemap')
+        this.sitemap = result.body
+
+    }
+
   }
+
 }
 </script>
 
