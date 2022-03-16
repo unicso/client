bc2c26e8bc2052491889b3f2a959a608bd184b48
root <dev@devcomp.ru>
2022-03-16 22:55:20 +0300

    Исправление букв в футере

--- components/main/FooterComponent.vue
+++ components/main/FooterComponent.vue
@@ -1,6 +1,5 @@
 <template>
 <footer class="content_block">
-asdsadsadsadasdsad
   <main>
   <section class="logotype">
     <nuxt-link to="/" v-html="$store.state.icons.logotype_vertical"></nuxt-link>
